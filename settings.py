5f219b1c4227c4fbb74fee4e03a20f46b74c0b9c
Yurii Bykov <jim95022@gmail.com>
2024-03-01 19:52:58 +0400

    - new source test files - new main.py - new functional tests

---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+
+BASE_URL = os.environ.get("BASE_URL", "")
+CONFIG_FILE = os.environ.get("CONFIG_FILE", "source/config.cfg")
+CONFIG_FOLDER = os.environ.get("CONFIG_FOLDER", "source/configs/")
---
+++ tests/functional/removing_existing_user.py
@@ -0,0 +1,9 @@
+# First case
+# - Get all users
+# - Remove existing user
+# - Get all users
+
+# Second case
+# - Get all users
+# - Remove not existing user
+# - Get all users
