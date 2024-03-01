393bd2ea1024e6b265feae7b64247abe47dd671f
Yurii Bykov <jim95022@gmail.com>
2024-03-01 20:15:57 +0400

    - new functional test adding_new_user - template for functional test retrieving_creds.py

---
+++ tests/functional/adding_new_user.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+
+import pytest
+
+from main import app
+from settings import CONFIG_FILE
+from fastapi.testclient import TestClient
+
+
+client = TestClient(app)
+username = "test_user"
+default_users = ["phone", "laptop", "desktop"]
+
+
+class TestNewUser:
+
+    @pytest.fixture
+    def temp_config(self, tmp_path):
+        base_dir = Path(__file__).parent.parent
+
+        config_template = base_dir / CONFIG_FILE
+        config_template_content = config_template.read_text()
+
+        temp_config = tmp_path / config_template.name
+        temp_config.write_text(config_template_content)
+
+        return temp_config
+
+    def test_adding_not_existing_user(self, temp_config):
+        # Get all users
+        response = client.get("/users")
+        assert response.status_code == 200
+        assert response.json() == {"users": default_users}
+
+        # Add new user
+        response = client.post("/users", data={"username": username})
+        assert response.status_code == 201
+
+        # Get all users
+        response = client.get("/users")
+        assert response.status_code == 200
+        assert response.json() == {"users": default_users + username}
+
+
+    def test_adding_existing_user(self, temp_config):
+        # Get all users
+        response = client.get("/users")
+        assert response.status_code == 200
+        assert response.json() == {"users": default_users}
+
+        # Add new user
+        response = client.post("/users", data={"username": username})
+        assert response.status_code == 201
+
+        # Get all users
+        response = client.get("/users")
+        assert response.status_code == 200
+        assert response.json() == {"users": default_users + username}
+
+        # Add the same user
+        response = client.post("/users", data={"username": username})
+        assert response.status_code == 400
+
+        # Get all users
+        response = client.get("/users")
+        assert response.status_code == 200
+        assert response.json() == {"users": default_users + username}
