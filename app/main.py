7f2439fb7c176e939f4907aef0c60346d84f9e72
Yurii Bykov <jim95022@gmail.com>
2024-03-02 21:21:18 +0400

    - update test_removing_user.py passed

--- app/main.py
+++ app/main.py
@@ -20,3 +20,11 @@ async def add_users(username: str):
         users.add(username)
     except ValueError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=repr(e))
+
+
+@app.delete("/users/{username}", status_code=status.HTTP_202_ACCEPTED)
+async def add_users(username: str):
+    try:
+        users.remove(username)
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=repr(e))
--- tests/functional/removing_existing_user.py
+++
@@ -1,9 +0,0 @@
-# First case
-# - Get all users
-# - Remove existing user
-# - Get all users
-
-# Second case
-# - Get all users
-# - Remove not existing user
-# - Get all users
